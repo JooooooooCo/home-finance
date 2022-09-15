814402e0d52869faf146f70e137f7260abe2450a
João Costa <joao.costa@amaiseducaca.com.br>
2022-09-15 00:59:51 +0000

    feat: add auth validation in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import { useAuthStore } from "@/store/auth.store";
 
 const routes = [
   {
@@ -8,16 +9,31 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
+    path: "/login",
+    name: "login",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ["/login"];
+
+  const authRequired = !publicPages.includes(to.path);
+
+  const authStore = useAuthStore();
+
+  if (authRequired && !authStore.isLoggedIn) {
+    authStore.returnUrl = to.path;
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
