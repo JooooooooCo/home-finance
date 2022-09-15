2db02ca4705d996e8320798cafb39bf0852fc525
João Costa <joao.costa@amaiseducaca.com.br>
2022-09-15 01:03:13 +0000

    feat: initial structure app.vue and main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import { createPinia } from "pinia";
 import router from "./router";
-import store from "./store";
+import App from "./App.vue";
+import "materialize-css/dist/css/materialize.min.css";
+import "material-design-icons/iconfont/material-icons.css";
+import "./assets/css/appMain.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+const pinia = createPinia();
+
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+app.mount("#app");
