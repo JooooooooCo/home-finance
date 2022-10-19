5a6eebc110ba62cb780089d2abca56f5fdaee5e3
João Costa <joao.costa@amaiseducaca.com.br>
2022-10-19 02:49:23 +0000

    fix: remove navbar from app main

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <NavBar v-if="enableNavBar" />
   <router-view />
 </template>
 
 <script>
 import M from "materialize-css";
-import NavBar from "@/components/NavBar.vue";
 
 export default {
-  components: {
-    NavBar,
-  },
-  computed: {
-    enableNavBar() {
-      const disabledNavRoutes = ["login", "cost-center-selection"];
-      return disabledNavRoutes.indexOf(this.$route.name) === -1;
-    },
-  },
   mounted() {
     M.AutoInit();
   },
