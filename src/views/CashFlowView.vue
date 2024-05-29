f8defca427f83dd171aeedf507d66d3ddf829191
JooooooooCo <costa.joaomf@gmail.com>
2024-05-28 23:59:03 -0300

    fix: add navbar on main views

--- src/views/CashFlowView.vue
+++ src/views/CashFlowView.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
+    <NavBar />
     <p>CashFlowView</p>
   </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
+
 export default {
   name: "CashFlowView",
+  components: {
+    NavBar,
+  },
 };
 </script>
 
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
+    <NavBar />
     <p>Dashboard</p>
   </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
+
 export default {
   name: "DashboardView",
+  components: {
+    NavBar,
+  },
 };
 </script>
 
