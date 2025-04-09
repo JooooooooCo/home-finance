d4f68d1d337a4ef66c1d6b0e18507b79d3e1020c
Joao Costa <costa.joaomf@gmail.com>
2025-04-09 08:49:34 -0300

    feat: add number stepper input comp

---
+++ src/components/generics/NumberStepperInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-text-field :label="label" :model-value="modelValue" variant="outlined"
+    @update:model-value="val => emit('update:modelValue', Number(val))">
+    <template #prepend-inner>
+      <v-btn icon size="small" variant="text" @click="decrement">
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+    </template>
+
+    <template #append-inner>
+      <v-btn icon size="small" variant="text" @click="increment">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </template>
+  </v-text-field>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: 1
+  },
+  label: {
+    type: String,
+    default: ""
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const increment = () => {
+  emit('update:modelValue', props.modelValue + 1)
+}
+
+const decrement = () => {
+  if (props.modelValue > 1) {
+    emit('update:modelValue', props.modelValue - 1)
+  }
+}
+</script>
