eef82ad9ea16da5a5a45382e82bf445b87b62f4b
Joao Costa <costa.joaomf@gmail.com>
2025-04-17 21:59:19 -0300

    feat: add loader and dialog loader components

---
+++ src/components/generics/LoaderCircular.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-progress-circular color="teal darken-2" indeterminate :size="size" class="ml-12 mr-12 mt-12 mb-12" />
+</template>
+
+<script setup>
+const props = defineProps({
+  size: {
+    type: Number,
+    default: 128
+  },
+});
+</script>
---
+++ src/components/generics/LoaderDialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-dialog :model-value="modelValue" max-width="80%" width="300" :persistent="persistent">
+    <LoaderCircular />
+  </v-dialog>
+</template>
+
+<script setup>
+import LoaderCircular from './LoaderCircular.vue';
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    required: true,
+  },
+  persistent: {
+    type: Boolean,
+    default: true
+  }
+});
+</script>
