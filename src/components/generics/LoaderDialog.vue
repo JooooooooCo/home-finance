6227bbd1b1c3ff165a5ab539fd53658f7d7103ff
Joao Costa <costa.joaomf@gmail.com>
2025-04-20 00:16:14 -0300

    feat: centralize loader dialog

--- src/components/generics/LoaderDialog.vue
+++ src/components/generics/LoaderDialog.vue
@@ -1,6 +1,8 @@
 <template>
   <v-dialog :model-value="modelValue" max-width="80%" width="300" :persistent="persistent">
-    <LoaderCircular />
+    <div class="w-auto d-flex justify-center">
+      <LoaderCircular />
+    </div>
   </v-dialog>
 </template>
 
