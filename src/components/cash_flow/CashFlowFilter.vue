b8bfc9c1c2b1f7b7ff8b5de29060bc0708b4fb25
Joao Costa <costa.joaomf@gmail.com>
2025-04-23 23:35:09 -0300

    feat: WIP add new filters on clash flow list

--- src/components/cash_flow/CashFlowFilter.vue
+++ src/components/cash_flow/CashFlowFilter.vue
@@ -15,9 +15,73 @@
 
                 <v-row class="mt-2">
                     <v-col cols="12" class="d-flex justify-center mb-3">
-                        <TransactionTypeSelector :multipleChoice="true" v-model="filters.transactionTypeIds" />
+                        <TransactionTypeSelector :multiple="true" v-model="filters.transactionTypeIds" />
                     </v-col>
                 </v-row>
+
+                <v-row class="mt-2">
+                    <v-col cols="12">
+                        <PaymentTypeSelectorMultiple v-model="filters.paymentTypeIds" />
+                    </v-col>
+                </v-row>
+
+                <v-row class="mt-2">
+                    <v-col cols="12">
+                        <PaymentStatusTypeSelectorMultiple v-model="filters.paymentStatusIds" />
+                    </v-col>
+                </v-row>
+
+                <v-row class="mt-2">
+                    <v-col cols="12">
+                        FILTROS DE DATA E VALOR
+                    </v-col>
+                </v-row>
+
+                <v-row class="mt-2">
+                    <v-col cols="12">
+                        <PrimaryCategorySelector v-model="filters.primaryCategoryId" />
+                    </v-col>
+                </v-row>
+
+                <v-row class="mt-2">
+                    <v-col cols="12">
+                        <SecondaryCategorySelector v-model="filters.secondaryCategoryId" :requireTransactionType="false" />
+                    </v-col>
+                </v-row>
+
+                <v-row class="mt-2">
+                    <v-col cols="12">
+                        <SpecificCategorySelector v-model="filters.specificCategoryId" :secondaryCategoryId="filters.secondaryCategoryId" />
+                    </v-col>
+                </v-row>
+
+                <v-row class="mt-2">
+                    <v-col cols="12">
+                        <v-textarea label="Descrição" v-model="filters.description" required rows="2" auto-grow variant="solo-filled" flat rounded-sm />
+                    </v-col>
+                </v-row>
+
+                <v-row class="mt-2">
+                    <v-col cols="12" class="mt-0">
+                        <v-checkbox
+                            v-model="filters.reconciled"
+                            color="teal darken-2"
+                            label="Conciliado"
+                        />
+                    </v-col>
+                </v-row>
+
+                <v-row class="mt-2">
+                    <v-col cols="12" class="mt-0">
+                        <v-checkbox
+                            v-model="filters.notReconciled"
+                            color="teal darken-2"
+                            label="Não conciliado"
+                        />
+                    </v-col>
+                </v-row>
+
+                filters: {{ filters }}
             </v-card>
         </v-dialog>
     </div>
@@ -26,6 +90,11 @@
 <script setup>
 import { ref, computed } from 'vue'
 import TransactionTypeSelector from '@/components/core/TransactionTypeSelector.vue';
+import PaymentTypeSelectorMultiple from '@/components/core/PaymentTypeSelectorMultiple.vue';
+import PaymentStatusTypeSelectorMultiple from '@/components/core/PaymentStatusTypeSelectorMultiple.vue';
+import PrimaryCategorySelector from '@/components/core/PrimaryCategorySelector.vue'
+import SecondaryCategorySelector from '@/components/core/SecondaryCategorySelector.vue'
+import SpecificCategorySelector from '@/components/core/SpecificCategorySelector.vue'
 const props = defineProps({
     modelValue: {
         type: Boolean,
@@ -33,7 +102,14 @@ const props = defineProps({
     },
 });
 const emptyFilterObject = {
-    transactionTypeIds: []
+    transactionTypeIds: [],
+    paymentTypeIds: [],Ids: [],
+    primaryCategoryId: null,
+    secondaryCategoryId: null,
+    specificCategoryId: null,
+    description: '',
+    reconciled: false,
+    notReconciled: false,
 }
 const filters = ref(emptyFilterObject);
 
