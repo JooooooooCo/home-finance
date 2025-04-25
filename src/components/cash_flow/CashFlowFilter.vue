d3925633dba19507593205f6903e504a3c85233b
Joao Costa <costa.joaomf@gmail.com>
2025-04-25 16:23:37 -0300

    feat: add date filters and improve design on filters

--- src/components/cash_flow/CashFlowFilter.vue
+++ src/components/cash_flow/CashFlowFilter.vue
@@ -8,8 +8,7 @@
                     <v-spacer />
 
                     <v-toolbar-items>
-                        <v-btn prepend-icon="mdi-check" text="Apply filters" variant="text"
-                            @click="applyFilters" />
+                        <v-btn prepend-icon="mdi-check" text="Apply filters" variant="text" @click="applyFilters" />
                     </v-toolbar-items>
                 </v-toolbar>
 
@@ -19,70 +18,98 @@
                             <TransactionTypeSelector :multiple="true" v-model="filters.transactionTypeIds" />
                         </v-col>
                     </v-row>
-    
+
                     <v-row>
                         <v-col cols="12">
                             <PaymentTypeSelectorMultiple v-model="filters.paymentTypeIds" />
                         </v-col>
                     </v-row>
-    
+
                     <v-row>
                         <v-col cols="12">
                             <PaymentStatusTypeSelectorMultiple v-model="filters.paymentStatusIds" />
                         </v-col>
                     </v-row>
-    
+
+                    <v-divider class="mt-8 mb-4" />
+
                     <v-row>
-                        <v-col cols="6">
-                            <v-text-field label="Valor Min" v-model="filters.amountMin" prefix="R$" type="number" variant="solo-filled" flat rounded-sm required hideDetails />
-                        </v-col>
-                        <v-col cols="6">
-                            <v-text-field label="Valor Max" v-model="filters.amountMax" prefix="R$" type="number" variant="solo-filled" flat rounded-sm required hideDetails />
+                        <v-col cols="12">
+                            <v-date-input label="Data de Vencimento" v-model="filters.dueDateRange" @click:clear="filters.dueDateRange = []"
+                                prepend-icon="" prepend-inner-icon="mdi-calendar" multiple="range" variant="solo-filled"
+                                flat clearable show-adjacent-months hide-details />
                         </v-col>
                     </v-row>
-    
+
+                    <v-row>
+                        <v-col cols="12">
+                            <v-date-input label="Data de Pagamento" v-model="filters.paymentDateRange" @click:clear="filters.paymentDateRange = []"
+                                prepend-icon="" prepend-inner-icon="mdi-calendar" multiple="range" variant="solo-filled"
+                                flat clearable show-adjacent-months hide-details />
+                        </v-col>
+                    </v-row>
+
+                    <v-row>
+                        <v-col cols="12">
+                            <v-date-input label="Data da Compra" v-model="filters.purchaseDateRange" @click:clear="filters.purchaseDateRange = []"
+                                prepend-icon="" prepend-inner-icon="mdi-calendar" multiple="range" variant="solo-filled"
+                                flat clearable show-adjacent-months hide-details />
+                        </v-col>
+                    </v-row>
+
+                    <v-divider class="mt-8 mb-4" />
+
+                    <v-row>
+                        <v-col cols="6">
+                            <v-text-field label="Valor Min" v-model="filters.amountMin" prefix="R$"
+                                prepend-inner-icon="mdi-currency-usd" type="number" variant="solo-filled" flat
+                                rounded-sm required hideDetails />
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field label="Valor Max" v-model="filters.amountMax" prefix="R$"
+                                prepend-inner-icon="mdi-currency-usd" type="number" variant="solo-filled" flat
+                                rounded-sm required hideDetails />
+                        </v-col>
+                    </v-row>
+
+                    <v-row>
+                        <v-col cols="12">
+                            <v-textarea label="Descrição" v-model="filters.description" required rows="2" auto-grow
+                                variant="solo-filled" flat rounded-sm hide-details />
+                        </v-col>
+                    </v-row>
+
+                    <v-divider class="mt-8 mb-4" />
+
                     <v-row>
                         <v-col cols="12">
                             <PrimaryCategorySelector v-model="filters.primaryCategoryId" hideDetails />
                         </v-col>
                     </v-row>
-    
+
                     <v-row>
                         <v-col cols="12">
-                            <SecondaryCategorySelector v-model="filters.secondaryCategoryId" :requireTransactionType="false" hideDetails />
+                            <SecondaryCategorySelector v-model="filters.secondaryCategoryId"
+                                :requireTransactionType="false" hideDetails />
                         </v-col>
                     </v-row>
-    
+
                     <v-row>
                         <v-col cols="12">
-                            <SpecificCategorySelector v-model="filters.specificCategoryId" :secondaryCategoryId="filters.secondaryCategoryId" hideDetails />
+                            <SpecificCategorySelector v-model="filters.specificCategoryId"
+                                :secondaryCategoryId="filters.secondaryCategoryId" hideDetails />
                         </v-col>
                     </v-row>
-    
-                    <v-row>
-                        <v-col cols="12">
-                            <v-textarea label="Descrição" v-model="filters.description" required rows="2" auto-grow variant="solo-filled" flat rounded-sm hide-details />
-                        </v-col>
-                    </v-row>
-    
+
                     <v-row>
                         <v-col cols="6" class="mt-0">
-                            <v-checkbox
-                                v-model="filters.reconciled"
-                                color="teal darken-2"
-                                label="Conciliado"
-                            />
+                            <v-checkbox v-model="filters.reconciled" color="teal darken-2" label="Conciliado" :true-value="1" :false-value="0" />
                         </v-col>
                         <v-col cols="6" class="mt-0">
-                            <v-checkbox
-                                v-model="filters.notReconciled"
-                                color="teal darken-2"
-                                label="Não conciliado"
-                            />
+                            <v-checkbox v-model="filters.notReconciled" color="teal darken-2" label="Não conciliado" :true-value="1" :false-value="0" />
                         </v-col>
                     </v-row>
-    
-                    filters: {{ filters }}
+
                 </v-col>
 
                 <v-footer color="teal darken-2">
@@ -97,6 +124,7 @@
 
 <script setup>
 import { ref, computed, onMounted } from 'vue'
+import { useDateHandler } from '@/composables/useDateHandler'
 import TransactionTypeSelector from '@/components/core/TransactionTypeSelector.vue';
 import PaymentTypeSelectorMultiple from '@/components/core/PaymentTypeSelectorMultiple.vue';
 import PaymentStatusTypeSelectorMultiple from '@/components/core/PaymentStatusTypeSelectorMultiple.vue';
@@ -110,9 +138,12 @@ const props = defineProps({
     },
 });
 const emptyFilterObject = {
-    transactionTypeIds: [1,2],
+    transactionTypeIds: [1, 2],
     paymentTypeIds: [],
     paymentStatusIds: [],
+    dueDateRange: [],
+    paymentDateRange: [],
+    purchaseDateRange: [],
     amountMin: '',
     amountMax: '',
     primaryCategoryId: null,
@@ -126,19 +157,31 @@ const filters = ref(emptyFilterObject);
 
 const emit = defineEmits(['update:modelValue', 'applyFilters', 'cancel'])
 
+const { apiDateFormatter, getMonthInitialEndDate } = useDateHandler();
+
 const isOpen = computed({
     get: () => props.modelValue,
     set: val => emit('update:modelValue', val),
 })
 
 const applyFilters = () => {
-    const selectedFilters = {
-        ... filters.value,
+    emit('applyFilters', prepareFiltersBeforeApply())
+    isOpen.value = false
+}
+
+const prepareFiltersBeforeApply = () => {
+    return {
+        ...filters.value,
         reconciled: filters.value.reconciled ? 1 : 0,
         notReconciled: filters.value.notReconciled ? 1 : 0,
+        dueDateRange: getRangeDateFormatted(filters.value.dueDateRange),
+        paymentDateRange: getRangeDateFormatted(filters.value.paymentDateRange),
+        purchaseDateRange: getRangeDateFormatted(filters.value.purchaseDateRange),
     }
-    emit('applyFilters', selectedFilters)
-    isOpen.value = false
+}
+
+const getRangeDateFormatted = (dateRange) => {
+    return dateRange.length > 0 ? [apiDateFormatter(dateRange.at(0)), apiDateFormatter(dateRange.at(-1))] : [];
 }
 
 const cancel = () => {
@@ -147,6 +190,7 @@ const cancel = () => {
 }
 
 onMounted(() => {
+    filters.value.dueDateRange = getMonthInitialEndDate(new Date());
     applyFilters();
 });
 </script>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,17 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { VDateInput } from 'vuetify/labs/VDateInput'
 import router from "./router";
 import App from "./App.vue";
 import "./assets/css/appMain.scss";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 const vuetify = createVuetify({
-  components,
+  components: {
+    ...components,
+    VDateInput,
+  },
   directives,
   icons: {
     defaultSet: "mdi",
