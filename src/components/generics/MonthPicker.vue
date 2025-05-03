44765ef0908e3598e93f9f85af23cf283e6182e4
Joao Costa <costa.joaomf@gmail.com>
2025-05-02 22:41:43 -0300

    feat: add month and year picker

---
+++ src/components/generics/MonthPicker.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <SelectPicker
+      v-model="selectedItem"
+      :items="availableOptions"
+      label="Mês"
+      @update:modelValue="changeSelection"
+      hideDetails
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import SelectPicker from '@/components/generics/SelectPicker.vue';
+
+const availableOptions = ref([
+  { id: 1, name: 'Janeiro' },
+  { id: 2, name: 'Fevereiro' },
+  { id: 3, name: 'Março' },
+  { id: 4, name: 'Abril' },
+  { id: 5, name: 'Maio' },
+  { id: 6, name: 'Junho' },
+  { id: 7, name: 'Julho' },
+  { id: 8, name: 'Agosto' },
+  { id: 9, name: 'Setembro' },
+  { id: 10, name: 'Outubro' },
+  { id: 11, name: 'Novembro' },
+  { id: 12, name: 'Dezembro' },
+]);
+
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: null
+  },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const selectedItem = ref(props.modelValue);
+
+const changeSelection = (value) => {
+  emit('update:modelValue', value);
+};
+</script>
---
+++ src/components/generics/YearPicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <SelectPicker
+      v-model="selectedItem"
+      :items="availableOptions"
+      label="Ano"
+      @update:modelValue="changeSelection"
+      hideDetails
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import SelectPicker from '@/components/generics/SelectPicker.vue';
+
+const availableOptions = ref(
+  Array.from({ length: 2050 - 2015 + 1 }, (_, i) => {
+    const year = 2015 + i;
+    return { id: year, name: String(year) };
+  })
+);
+
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: null
+  },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const selectedItem = ref(props.modelValue);
+
+const changeSelection = (value) => {
+  emit('update:modelValue', value);
+};
+</script>
